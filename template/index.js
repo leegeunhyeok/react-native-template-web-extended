32826ed626ca2c96ca51c8a46c6d064b0f5a54b6
leegeunhyeok <dev.ghlee@gmail.com>
2023-07-21 19:35:50 +0900

    chore: add comment to entry point

--- template/index.js
+++ template/index.js
@@ -3,6 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
+/**
+ * should keep same path with `tsconfig.json` and `babel.config.js`
+ */
 import {App} from '@/App';
 import {name as appName} from './app.json';
 
