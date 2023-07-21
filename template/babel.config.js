1d838f3c9f02c122fda9ac75e7cc3ae8295ea24e
leegeunhyeok <dev.ghlee@gmail.com>
2023-07-21 19:37:03 +0900

    fix: compile error with using some libraries

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,31 +1,34 @@
-const nativeEntryPoint = '@/App';
-const platform = process.env.PLATFORM;
-const isStorybook = process.env.STORYBOOK === '1';
-const isNativeStorybook = isStorybook && platform === 'native';
-
 /**
  * @param {import('@babel/core').ConfigAPI} api
  */
 module.exports = api => {
-  api.cache.using(() => JSON.stringify({platform, isStorybook}));
+  const isWeb = api.caller(({target}) => target === 'web');
+  const isStorybook = process.env.STORYBOOK === '1';
+  const isNativeStorybook = !isWeb && isStorybook;
+  api.cache.using(() => JSON.stringify({isWeb, isStorybook}));
 
   return {
-    presets: ['module:metro-react-native-babel-preset'],
+    presets: [
+      'module:metro-react-native-babel-preset',
+      {
+        // to avoid duplicate plugin error in react-scripts
+        // false (default): using `@babel/plugin-transform-react-jsx`
+        // true: not using `@babel/plugin-transform-react-jsx`
+        useTransformReactJSXExperimental: isWeb,
+      },
+    ],
     plugins: [
-      platform === 'web'
-        ? ['babel-plugin-react-native-web', {commonjs: true}]
-        : null,
       [
         'module-resolver',
         {
           root: ['.'],
           alias: {
-            [nativeEntryPoint]: isNativeStorybook
-              ? './src/App.storybook'
-              : './src/App',
+            '@/App': isNativeStorybook ? './src/App.storybook' : './src/App',
           },
         },
       ],
+      isWeb ? ['babel-plugin-react-native-web', {commonjs: true}] : null,
+      // additional plugins here
     ].filter(Boolean),
   };
 };
