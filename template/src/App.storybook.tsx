556f725e4e5edb58441a713c9c92c0a282161399
leegeunhyeok <dev.ghlee@gmail.com>
2023-07-21 19:43:19 +0900

    fix: change storybook directory name

--- template/src/App.storybook.tsx
+++ template/src/App.storybook.tsx
@@ -1,6 +1,6 @@
 /**
  * Native storybook entry point
  */
-import StorybookUIRoot from '../.ondevice/Storybook';
+import StorybookUIRoot from '../.storybook-native/Storybook';
 
 export const App = StorybookUIRoot;
